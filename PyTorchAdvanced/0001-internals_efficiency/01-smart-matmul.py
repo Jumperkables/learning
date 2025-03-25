d53ce1c286afbddc59e86fdfba91d6dac22d5e2b
Jumperkables <tom.winterbottom@msn.com>
2025-03-25 16:01:09 +0000

    figured out why 2 bits not 3 bits in 2-4

--- PyTorchAdvanced/0001-internals_efficiency/01-smart-matmul.py
+++ PyTorchAdvanced/0001-internals_efficiency/01-smart-matmul.py
@@ -3,6 +3,8 @@ import torch
 # Make a well distributed 2x2 Matrix with given sparsity
 sparsity = 0.8
 well_dist = torch.threshold(torch.rand(6400,6400), sparsity, 0)
-well_dist_coo = A.to_sparse(torch.sparse_coo)   # Co-ordinate rep
-well_dist_csr = A.to_sparse(torch.sparse_csr)   # Compressed row format
-well_dist_bsr = A.to_sparse(torch.sparse_bsr)   # Block compressed sparse row format
+well_dist_csr = well_dist.to_sparse(layout=torch.sparse_csr)   # Compressed row format
+well_dist_csc = well_dist.to_sparse(layout=torch.sparse_csc)   # Compressed column format
+well_dist_coo = well_dist.to_sparse(layout=torch.sparse_coo)   # Co-ordinate rep
+breakpoint()
+well_dist_bsr = well_dist.to_sparse(layout=torch.sparse_bsr)   # Block compressed sparse row format
